327574364f58a420aba5dab1b14e9a0731e3d09c
Christian Hampel <80498051+chrishampel88@users.noreply.github.com>
2022-04-04 13:59:17 -0300

    e

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -14,7 +14,7 @@ export default function Contact(){
                 <a className={style.link} rel="noreferrer" href="https://github.com/chrishampel88" target="_blank"> Github</a>
             </h2> 
             <h2 >
-                <img className={style.logo} src="https://cdn-icons.flaticon.com/png/512/3178/premium/3178283.png?token=exp=1648751792~hmac=4f52271186d71acff5a7aaab2926bb0c" alt="" />
+                <img className={style.logo} src="https://cdn-icons-png.flaticon.com/128/2164/2164894.png" alt="" />
                 <a className={style.link} rel="noreferrer" href="mailto:christianhampel88@gmail.com" target="_blank"> E-mail</a>
             </h2>
         </div>
