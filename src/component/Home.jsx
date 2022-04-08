6110cb468deec2d6275cf7524ea1a323bc6281b5
Christian Hampel <80498051+chrishampel88@users.noreply.github.com>
2022-04-07 21:49:40 -0300

    a

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -21,7 +21,7 @@ try{
     else{
       const ciudad={
         id: recurso.id,
-        temp: recurso.main.temp,
+        temp: Math.floor(recurso.main.temp),
         name: recurso.name,
         min: Math.floor(recurso.main.temp_min),
         max: Math.floor(recurso.main.temp_max),
