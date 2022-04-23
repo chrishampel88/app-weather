5af7caab6ccea6f013c23a9e681b261718393f65
Christian Hampel <80498051+chrishampel88@users.noreply.github.com>
2022-04-22 21:29:28 -0300

    da

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,19 +1,20 @@
 import {Navbar, Nav, Container} from "react-bootstrap";
+import style from '../styles/Navbar.module.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import '../styles/Navbar.css'
+
 
 export default function NavBar(){
 
     return(
         <>
         <Navbar bg="dark" variant="dark">
-          <Container className="nav-container">
-          <Navbar.Brand className="nav-title">My Weather App</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/Me">About me</Nav.Link>
-            <Nav.Link href="/Contact">Contact</Nav.Link>
-          </Nav>
+          <Container className={style.navContainer}>
+            <Navbar.Brand className={style.navTitle}>My Weather App</Navbar.Brand>
+            <Nav className={style.meAuto}>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/Me">About me</Nav.Link>
+              <Nav.Link href="/Contact">Contact</Nav.Link>
+            </Nav>
           </Container>
         </Navbar>      
       </>   
